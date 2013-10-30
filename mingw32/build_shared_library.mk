2e9304bcddf4604dffc8223d82cc9e85acd8ff51
shun.zhang <aaashun@gmail.com>
2013-10-30 11:20:35 +0800

    Update build_shared_library.mk

--- mingw32/build_shared_library.mk
+++ mingw32/build_shared_library.mk
@@ -1,5 +1,5 @@
-ifeq ($(LOCAL_DLL_NAME_IN_LIB),)
-    LOCAL_DLL_NAME_IN_LIB = $(LOCAL_MODULE).dll
+ifeq ($(LOCAL_DLLNAME),)
+    LOCAL_DLLNAME = $(LOCAL_MODULE).dll
 endif
 LDFLAGS := -shared $(LDFLAGS) #-Wl,--enable-stdcall-fixup
 ALL = $(SHARED_LIBRARY)
